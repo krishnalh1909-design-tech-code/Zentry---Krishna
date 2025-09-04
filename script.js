30fb1a345bec7a160f5fae73724f300138a55eb5
krishnalh1909-design-tech-code <krishnalh1909@gmail.com>
2025-09-04 19:14:17 +0530

    loader added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Always scroll to top on refresh
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
+
+// Fade out loader after 5 seconds
+setTimeout(() => {
+  gsap.to(".loader", {
+    opacity: 0,
+    duration: 0.6,
+    ease: "power2.out",
+    onComplete: () => {
+      document.querySelector(".loader").style.display = "none";
+    },
+  });
+}, 5000);
+
